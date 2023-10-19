80673dca6ddbf5df0326f098a598b7a582faaf40
Gabe Gaynor <ggaynor@bremc.com>
2023-10-18 21:51:47 -0400

    ML model changes

--- F1_decision_tree.py
+++ F1_decision_tree.py
@@ -27,13 +27,28 @@ model = tree.DecisionTreeClassifier()
 # Fit the model to your data
 model.fit(X, y)
 
-# use the model to make predictions on new data
-# Have not done this either
-
-# Preprocess the input data for the upcoming race in Las Vegas
-upcoming_race_data = {'DRIVER': ['L HAMILTON'], 'TEAM': ['Mercedes'], 'CIRCUIT_TYPE': ['Street']}
-upcoming_race_df = pd.DataFrame(upcoming_race_data)
-upcoming_race_df = pd.get_dummies(upcoming_race_df)
+# mark a 1 for the values we would like to predict, refer to the table to know which circuit type to use. 
+# for example Albon = 1, Williams = 1 and Street type circuit = 1 for las vegas. Alex Albon would be projected 14th.
+upcoming_race_df = pd.DataFrame({
+    'DRIVER_A ALBON': [0], 'DRIVER_A GIOVINAZZI': [0],
+    'DRIVER_C LECLERC': [0], 'DRIVER_C SAINZ': [0], 'DRIVER_D KVYAT': [0],
+    'DRIVER_D RICCIARDO': [0], 'DRIVER_E OCON': [0], 'DRIVER_F ALONSO': [0],
+    'DRIVER_G RUSSELL': [0], 'DRIVER_G ZHOU': [0], 'DRIVER_K MAGNUSSEN': [0],
+    'DRIVER_K RAIKKONEN': [0],'DRIVER_L HAMILTON': [0], 'DRIVER_L LAWSON': [0], 'DRIVER_L NORRIS': [0],
+    'DRIVER_L SARGEANT': [0], 'DRIVER_L STROLL': [0], 'DRIVER_M SCHUMACHER': [0],
+    'DRIVER_M VERSTAPPEN': [1], 'DRIVER_N DE VRIES': [0], 'DRIVER_N HULKENBERG': [0],
+    'DRIVER_N LATIFI': [0], 'DRIVER_N MAZEPIN': [0], 'DRIVER_O PIASTRI': [0],
+    'DRIVER_P FITTIPALDI': [0], 'DRIVER_P GASLY': [0], 'DRIVER_R GROSJEAN': [0],
+    'DRIVER_R KUBICA': [0], 'DRIVER_S PEREZ': [0], 'DRIVER_S VETTEL': [0],
+    'DRIVER_V BOTTAS': [0],'DRIVER_Y TSUNODA': [0], 'DRIVER_nan': [0],
+    'TEAM_Alfa Romeo': [0], 'TEAM_Alfa Romeo Racing': [0], 'TEAM_AlphaTauri':[0],
+    'TEAM_Alpine':[0], 'TEAM_Aston Martin': [0], 'TEAM_Ferrari': [0], 'TEAM_Haas F1 Team': [0],
+    'TEAM_McLaren': [0], 'TEAM_Mercedes': [0], 'TEAM_Racing Point': [0], 'TEAM_Red Bull Racing': [1], 
+    'TEAM_Renault': [0], 'TEAM_Williams': [1], 'TEAM_nan': [0],
+    'CIRCUIT_TYPE_High Speed': [0], 'CIRCUIT_TYPE_High-Elevation': [0],
+    'CIRCUIT_TYPE_Intermediate': [0], 'CIRCUIT_TYPE_Low-Speed': [0],
+    'CIRCUIT_TYPE_Other': [0], 'CIRCUIT_TYPE_Street': [1]
+})
 
 # Make predictions using the trained model
 predicted_positions = model.predict(upcoming_race_df)
