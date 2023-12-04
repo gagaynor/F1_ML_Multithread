178f8939befcb0197691ca694bbccaac2695e7b6
Gabe Gaynor <ggaynor@bremc.com>
2023-12-04 11:30:49 -0500

    add multithreading to the code

--- F1_decision_tree.py
+++ F1_decision_tree.py
@@ -12,6 +12,23 @@ import os
 
 from concurrent.futures import ThreadPoolExecutor
 
+def process_model(number_cpus, model_idx, model, X, y, upcoming_race_df):
+    t1_start = perf_counter()
+    model.fit(X, y)
+    predicted_positions = model.predict(upcoming_race_df)
+    scores = cross_val_score(estimator=model, X=X, y=y, cv=7)
+    t1_stop = perf_counter()
+
+    new_row = {
+        'Model': f'Model {model_idx}',
+        'CPUs_used': number_cpus,
+        'Execution_time': round((t1_stop - t1_start), 4),
+        'Prediction': predicted_positions[0],
+        'Score': round(scores.mean(), 4)
+    }
+
+    return new_row
+
 
 def main():
     cpu_count = os.cpu_count()
@@ -45,82 +62,62 @@ def main():
 
     # Create empty dataframe to store the execution times of each loop
 
-    results_df = pd.DataFrame(columns=['CPUs_used', 'Execution_time', 'Prediction', 'Score'])
+    results_df = pd.DataFrame(columns=['Model','CPUs_used', 'Execution_time', 'Prediction', 'Score'])
 
     # print ('Results using RandomForestClassifier() model.')    
     # print ('Results using KNeighborsClassifier() model.')
-    print ('Results using SGDClassifier() model.')
+    # print ('Results using SGDClassifier() model.')
 
     for number_cpus in range(1, cpu_count):
+        with ThreadPoolExecutor(max_workers=number_cpus) as executor:
+            futures = []
 
-        models = [
-            RandomForestClassifier(n_jobs=number_cpus),
-            KNeighborsClassifier(n_neighbors=5,n_jobs=number_cpus),
-            SGDClassifier(n_jobs=number_cpus)
-        ]
+            models = [
+                RandomForestClassifier(n_jobs=number_cpus),
+                KNeighborsClassifier(n_neighbors=5,n_jobs=number_cpus),
+                SGDClassifier(n_jobs=number_cpus)
+            ]
 
-        # model = RandomForestClassifier(n_jobs=number_cpus)
+            # model = RandomForestClassifier(n_jobs=number_cpus)
 
-        # model = KNeighborsClassifier(n_jobs=number_cpus)
+            # model = KNeighborsClassifier(n_jobs=number_cpus)
 
-        # model = SGDClassifier(n_jobs=number_cpus)
+            # model = SGDClassifier(n_jobs=number_cpus)
 
-        for model_inx, model in enumerate(models, start=1):
-            # start counter to measure execution time
-            t1_start = perf_counter() 
+            for model_inx, model in enumerate(models, start=1):
+                
+                # mark a 1 for the values we would like to predict, refer to the table to know which circuit type to use. 
+                # for example Albon = 1, Williams = 1 and Street type circuit = 1 for las vegas. Alex Albon would be projected 14th.
+                upcoming_race_df = pd.DataFrame({
+                    'DRIVER_A ALBON': [1], 'DRIVER_A GIOVINAZZI': [0],
+                    'DRIVER_C LECLERC': [0], 'DRIVER_C SAINZ': [0], 'DRIVER_D KVYAT': [0],
+                    'DRIVER_D RICCIARDO': [0], 'DRIVER_E OCON': [0], 'DRIVER_F ALONSO': [0],
+                    'DRIVER_G RUSSELL': [0], 'DRIVER_G ZHOU': [0], 'DRIVER_K MAGNUSSEN': [0],
+                    'DRIVER_K RAIKKONEN': [0],'DRIVER_L HAMILTON': [0], 'DRIVER_L LAWSON': [0], 'DRIVER_L NORRIS': [0],
+                    'DRIVER_L SARGEANT': [0], 'DRIVER_L STROLL': [0], 'DRIVER_M SCHUMACHER': [0],
+                    'DRIVER_M VERSTAPPEN': [0], 'DRIVER_N DE VRIES': [0], 'DRIVER_N HULKENBERG': [0],
+                    'DRIVER_N LATIFI': [0], 'DRIVER_N MAZEPIN': [0], 'DRIVER_O PIASTRI': [0],
+                    'DRIVER_P FITTIPALDI': [0], 'DRIVER_P GASLY': [0], 'DRIVER_R GROSJEAN': [0],
+                    'DRIVER_R KUBICA': [0], 'DRIVER_S PEREZ': [0], 'DRIVER_S VETTEL': [0],
+                    'DRIVER_V BOTTAS': [0],'DRIVER_Y TSUNODA': [0], 'DRIVER_nan': [0],
+                    'TEAM_Alfa Romeo': [0], 'TEAM_Alfa Romeo Racing': [0], 'TEAM_AlphaTauri':[1],
+                    'TEAM_Alpine':[0], 'TEAM_Aston Martin': [0], 'TEAM_Ferrari': [0], 'TEAM_Haas F1 Team': [0],
+                    'TEAM_McLaren': [0], 'TEAM_Mercedes': [0], 'TEAM_Racing Point': [0], 'TEAM_Red Bull Racing': [1], 
+                    'TEAM_Renault': [0], 'TEAM_Williams': [1], 'TEAM_nan': [0],
+                    'CIRCUIT_TYPE_High Speed': [0], 'CIRCUIT_TYPE_High-Elevation': [0],
+                    'CIRCUIT_TYPE_Intermediate': [0], 'CIRCUIT_TYPE_Low-Speed': [0],
+                    'CIRCUIT_TYPE_Other': [0], 'CIRCUIT_TYPE_Street': [1]
+                })
 
-            # Fit the model to your data
-            model.fit(X, y)
+                futures.append(
+                    executor.submit(process_model, number_cpus, model_inx, model, X, y, upcoming_race_df)
+                )
 
-            # mark a 1 for the values we would like to predict, refer to the table to know which circuit type to use. 
-            # for example Albon = 1, Williams = 1 and Street type circuit = 1 for las vegas. Alex Albon would be projected 14th.
-            upcoming_race_df = pd.DataFrame({
-                'DRIVER_A ALBON': [1], 'DRIVER_A GIOVINAZZI': [0],
-                'DRIVER_C LECLERC': [0], 'DRIVER_C SAINZ': [0], 'DRIVER_D KVYAT': [0],
-                'DRIVER_D RICCIARDO': [0], 'DRIVER_E OCON': [0], 'DRIVER_F ALONSO': [0],
-                'DRIVER_G RUSSELL': [0], 'DRIVER_G ZHOU': [0], 'DRIVER_K MAGNUSSEN': [0],
-                'DRIVER_K RAIKKONEN': [0],'DRIVER_L HAMILTON': [0], 'DRIVER_L LAWSON': [0], 'DRIVER_L NORRIS': [0],
-                'DRIVER_L SARGEANT': [0], 'DRIVER_L STROLL': [0], 'DRIVER_M SCHUMACHER': [0],
-                'DRIVER_M VERSTAPPEN': [0], 'DRIVER_N DE VRIES': [0], 'DRIVER_N HULKENBERG': [0],
-                'DRIVER_N LATIFI': [0], 'DRIVER_N MAZEPIN': [0], 'DRIVER_O PIASTRI': [0],
-                'DRIVER_P FITTIPALDI': [0], 'DRIVER_P GASLY': [0], 'DRIVER_R GROSJEAN': [0],
-                'DRIVER_R KUBICA': [0], 'DRIVER_S PEREZ': [0], 'DRIVER_S VETTEL': [0],
-                'DRIVER_V BOTTAS': [0],'DRIVER_Y TSUNODA': [0], 'DRIVER_nan': [0],
-                'TEAM_Alfa Romeo': [0], 'TEAM_Alfa Romeo Racing': [0], 'TEAM_AlphaTauri':[1],
-                'TEAM_Alpine':[0], 'TEAM_Aston Martin': [0], 'TEAM_Ferrari': [0], 'TEAM_Haas F1 Team': [0],
-                'TEAM_McLaren': [0], 'TEAM_Mercedes': [0], 'TEAM_Racing Point': [0], 'TEAM_Red Bull Racing': [1], 
-                'TEAM_Renault': [0], 'TEAM_Williams': [1], 'TEAM_nan': [0],
-                'CIRCUIT_TYPE_High Speed': [0], 'CIRCUIT_TYPE_High-Elevation': [0],
-                'CIRCUIT_TYPE_Intermediate': [0], 'CIRCUIT_TYPE_Low-Speed': [0],
-                'CIRCUIT_TYPE_Other': [0], 'CIRCUIT_TYPE_Street': [1]
-            })
-
-            # Make predictions using the trained model
-            predicted_positions = model.predict(upcoming_race_df)
-
-            # Print the predicted positions for the upcoming race
-            # print("Predicted positions for the upcoming race in Las Vegas:")
-            # print(predicted_positions)  
-
-            scores = cross_val_score(estimator=model, X=X, y=y, cv=7)
-            # print (f'Classification model average cross val score: {scores.mean():.04f}')
-
-
-            # Stop the stopwatch / counter
-            t1_stop = perf_counter()
-
-            # create new row to append
-            new_row = { 'Model': f'Model {model_inx}',
-                        'CPUs_used':number_cpus ,
-                        'Execution_time': round((t1_stop-t1_start), 4), 
-                        'Prediction':predicted_positions[0],
-                        'Score': round(scores.mean(), 4)}
-
-            # add row to dataframe
-            results_df = pd.concat([results_df, pd.DataFrame([new_row])], ignore_index=True)
-
-            print (f'Execution with Model {model_inx} and {number_cpus} cpus complete.')
-
+                for future in futures:
+                    result = future.result()
+                    results_df = pd.concat([results_df, pd.DataFrame([result])], ignore_index=True)
+                    print(f'Execution with {result["Model"]} and {result["CPUs_used"]} cpus complete.')
+                
     # results_df.style.hide()
 
     results_df.set_index('CPUs_used', inplace=True)
