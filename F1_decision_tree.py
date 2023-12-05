bee991c281f8a10fe1dc592f75730ddae0ebcd72
Paul Miller <pdmill@gmail.com>
2023-12-04 22:41:50 -0800

    Tweaked multiprocessing loops and saving figures to disk

--- F1_decision_tree.py
+++ F1_decision_tree.py
@@ -14,15 +14,19 @@ from concurrent.futures import ThreadPoolExecutor
 
 import matplotlib.pyplot as plt
 
-def process_model(number_cpus, model_idx, model, X, y, upcoming_race_df):
+def process_model(args):
+
+    number_cpus, model_name, model, X, y, upcoming_race_df = args
+    
     t1_start = perf_counter()
+    
     model.fit(X, y)
     predicted_positions = model.predict(upcoming_race_df)
     scores = cross_val_score(estimator=model, X=X, y=y, cv=7)
     t1_stop = perf_counter()
 
     new_row = {
-        'Model': f'Model {model_idx}',
+        'Model': f'{model_name}',
         'CPUs_used': number_cpus,
         'Execution_time': round((t1_stop - t1_start), 4),
         'Prediction': predicted_positions[0],
@@ -33,6 +37,13 @@ def process_model(number_cpus, model_idx, model, X, y, upcoming_race_df):
 
 
 def main():
+    '''
+    Loop through a range of 1 up to the max number of cpus on a machine.
+    In each loop, call three models using an increading number of workers (one worker for each cpu)
+
+    The n_jobs parameter is set to 1 for each model.
+    '''
+
     cpu_count = os.cpu_count()
 
     print (f'Number of cpus in system: {cpu_count}')
@@ -57,70 +68,91 @@ def main():
     # Convert categorical variables into numerical values using one-hot encoding
     X = pd.get_dummies(X)
 
-    # Change n_jobs to modify how many CPU's are being utilized
-    # model = tree.DecisionTreeClassifier()
-
-    # Loop through using 1 to total number of cpus in the RandomForestClassifier(njobs) parameter
-
     # Create empty dataframe to store the execution times of each loop
-
     results_df = pd.DataFrame(columns=['Model','CPUs_used', 'Execution_time', 'Prediction', 'Score'])
 
-    # print ('Results using RandomForestClassifier() model.')    
-    # print ('Results using KNeighborsClassifier() model.')
-    # print ('Results using SGDClassifier() model.')
+        # Create empty dataframe to store the execution times of each loop
+    summary_results_df = pd.DataFrame(columns=['Model','CPUs_used', 'Execution_time'])
 
-    for number_cpus in range(1, cpu_count):
-        with ThreadPoolExecutor(max_workers=number_cpus) as executor:
-            futures = []
+    # Initialize untrained models to pass into the process_model function
+    # Set n_jobs to 1. This ensures the thread pool is directing traffic.
+    models = [{'name':"Random_Forest", 'model': RandomForestClassifier(n_jobs=1)},
+        {'name':"KNeighbors", 'model': KNeighborsClassifier(n_neighbors=5,n_jobs=1)},
+        {'name':"SGD", 'model': SGDClassifier(n_jobs=1)}
+    ]
 
-            models = [
-                RandomForestClassifier(n_jobs=number_cpus),
-                KNeighborsClassifier(n_neighbors=5,n_jobs=number_cpus),
-                SGDClassifier(n_jobs=number_cpus)
-            ]
+    # Create results list for all output
+    results = []
 
-            # model = RandomForestClassifier(n_jobs=number_cpus)
+    # Loop through using 1 to total number of cpus. Add 1 since range (1, 12) is not inclusive of the 12
+    for number_cpus in range(1, cpu_count + 1): 
 
-            # model = KNeighborsClassifier(n_jobs=number_cpus)
 
-            # model = SGDClassifier(n_jobs=number_cpus)
+        # Keep track of how much time it took to do all three models
+        total_time_start = perf_counter()
 
-            for model_inx, model in enumerate(models, start=1):
-                
-                # mark a 1 for the values we would like to predict, refer to the table to know which circuit type to use. 
-                # for example Albon = 1, Williams = 1 and Street type circuit = 1 for las vegas. Alex Albon would be projected 14th.
-                upcoming_race_df = pd.DataFrame({
-                    'DRIVER_A ALBON': [1], 'DRIVER_A GIOVINAZZI': [0],
-                    'DRIVER_C LECLERC': [0], 'DRIVER_C SAINZ': [0], 'DRIVER_D KVYAT': [0],
-                    'DRIVER_D RICCIARDO': [0], 'DRIVER_E OCON': [0], 'DRIVER_F ALONSO': [0],
-                    'DRIVER_G RUSSELL': [0], 'DRIVER_G ZHOU': [0], 'DRIVER_K MAGNUSSEN': [0],
-                    'DRIVER_K RAIKKONEN': [0],'DRIVER_L HAMILTON': [0], 'DRIVER_L LAWSON': [0], 'DRIVER_L NORRIS': [0],
-                    'DRIVER_L SARGEANT': [0], 'DRIVER_L STROLL': [0], 'DRIVER_M SCHUMACHER': [0],
-                    'DRIVER_M VERSTAPPEN': [0], 'DRIVER_N DE VRIES': [0], 'DRIVER_N HULKENBERG': [0],
-                    'DRIVER_N LATIFI': [0], 'DRIVER_N MAZEPIN': [0], 'DRIVER_O PIASTRI': [0],
-                    'DRIVER_P FITTIPALDI': [0], 'DRIVER_P GASLY': [0], 'DRIVER_R GROSJEAN': [0],
-                    'DRIVER_R KUBICA': [0], 'DRIVER_S PEREZ': [0], 'DRIVER_S VETTEL': [0],
-                    'DRIVER_V BOTTAS': [0],'DRIVER_Y TSUNODA': [0], 'DRIVER_nan': [0],
-                    'TEAM_Alfa Romeo': [0], 'TEAM_Alfa Romeo Racing': [0], 'TEAM_AlphaTauri':[1],
-                    'TEAM_Alpine':[0], 'TEAM_Aston Martin': [0], 'TEAM_Ferrari': [0], 'TEAM_Haas F1 Team': [0],
-                    'TEAM_McLaren': [0], 'TEAM_Mercedes': [0], 'TEAM_Racing Point': [0], 'TEAM_Red Bull Racing': [1], 
-                    'TEAM_Renault': [0], 'TEAM_Williams': [1], 'TEAM_nan': [0],
-                    'CIRCUIT_TYPE_High Speed': [0], 'CIRCUIT_TYPE_High-Elevation': [0],
-                    'CIRCUIT_TYPE_Intermediate': [0], 'CIRCUIT_TYPE_Low-Speed': [0],
-                    'CIRCUIT_TYPE_Other': [0], 'CIRCUIT_TYPE_Street': [1]
-                })
+        for named_model in models:
 
-                futures.append(
-                    executor.submit(process_model, number_cpus, model_inx, model, X, y, upcoming_race_df)
-                )
+            model_name = named_model.get('name')
+            model = named_model.get('model')
 
-                for future in futures:
+            # Test data to use for prediction:
+            # mark a 1 for the values we would like to predict, refer to the table to know which circuit type to use. 
+            # for example Albon = 1, Williams = 1 and Street type circuit = 1 for las vegas. Alex Albon would be projected 14th.
+            upcoming_race_df = pd.DataFrame({
+                'DRIVER_A ALBON': [1], 'DRIVER_A GIOVINAZZI': [0],
+                'DRIVER_C LECLERC': [0], 'DRIVER_C SAINZ': [0], 'DRIVER_D KVYAT': [0],
+                'DRIVER_D RICCIARDO': [0], 'DRIVER_E OCON': [0], 'DRIVER_F ALONSO': [0],
+                'DRIVER_G RUSSELL': [0], 'DRIVER_G ZHOU': [0], 'DRIVER_K MAGNUSSEN': [0],
+                'DRIVER_K RAIKKONEN': [0],'DRIVER_L HAMILTON': [0], 'DRIVER_L LAWSON': [0], 'DRIVER_L NORRIS': [0],
+                'DRIVER_L SARGEANT': [0], 'DRIVER_L STROLL': [0], 'DRIVER_M SCHUMACHER': [0],
+                'DRIVER_M VERSTAPPEN': [0], 'DRIVER_N DE VRIES': [0], 'DRIVER_N HULKENBERG': [0],
+                'DRIVER_N LATIFI': [0], 'DRIVER_N MAZEPIN': [0], 'DRIVER_O PIASTRI': [0],
+                'DRIVER_P FITTIPALDI': [0], 'DRIVER_P GASLY': [0], 'DRIVER_R GROSJEAN': [0],
+                'DRIVER_R KUBICA': [0], 'DRIVER_S PEREZ': [0], 'DRIVER_S VETTEL': [0],
+                'DRIVER_V BOTTAS': [0],'DRIVER_Y TSUNODA': [0], 'DRIVER_nan': [0],
+                'TEAM_Alfa Romeo': [0], 'TEAM_Alfa Romeo Racing': [0], 'TEAM_AlphaTauri':[1],
+                'TEAM_Alpine':[0], 'TEAM_Aston Martin': [0], 'TEAM_Ferrari': [0], 'TEAM_Haas F1 Team': [0],
+                'TEAM_McLaren': [0], 'TEAM_Mercedes': [0], 'TEAM_Racing Point': [0], 'TEAM_Red Bull Racing': [1], 
+                'TEAM_Renault': [0], 'TEAM_Williams': [1], 'TEAM_nan': [0],
+                'CIRCUIT_TYPE_High Speed': [0], 'CIRCUIT_TYPE_High-Elevation': [0],
+                'CIRCUIT_TYPE_Intermediate': [0], 'CIRCUIT_TYPE_Low-Speed': [0],
+                'CIRCUIT_TYPE_Other': [0], 'CIRCUIT_TYPE_Street': [1]
+            })
+
+
+
+            # Create a pool with an increasing number or workers
+            with ThreadPoolExecutor(max_workers=number_cpus) as executor:
+
+                    # futures.append(
+                    future = executor.submit(process_model, (number_cpus, model_name, model, X, y, upcoming_race_df))
+                    # )
+
                     result = future.result()
-                    results_df = pd.concat([results_df, pd.DataFrame([result])], ignore_index=True)
-                    print(f'Execution with {result["Model"]} and {result["CPUs_used"]} cpus complete.')
+
+                    results.append(result)
+
+        total_time_stop = perf_counter()
+
+        # Add a row for the total time to train and predict with all three models
+        summary_row = {
+        'Model': 'All three models',
+        'CPUs_used': number_cpus,
+        'Execution_time': round((total_time_stop - total_time_start), 4),
+        'Prediction': '',
+        'Score': ''
+        }
+
+        summary_results_df = pd.concat([summary_results_df, pd.DataFrame([summary_row])], ignore_index=True)
+
+
+
+    for result in results:
+        results_df = pd.concat([results_df, pd.DataFrame([result])], ignore_index=True)
+        print(f'Execution with {result["Model"]} and {result["CPUs_used"]} cpus complete.')
                 
-    # results_df.style.hide()
+
 
     results_df.set_index('CPUs_used', inplace=True)
 
@@ -143,12 +175,16 @@ def main():
     plt.grid(axis='y')
 
     # Show plot
-    plt.show()
+    # plt.show()
+    
+    # Save the plot to an image file
+    plt.savefig('sum_exec_time.png', bbox_inches="tight")
+
 
     sum_exec_time_models = results_df.groupby('Model')['Execution_time'].sum()
 
     # Plotting
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(10, 8))
     plt.bar(sum_exec_time_models.index, sum_exec_time_models.values, color='red')
     plt.xlabel('Models')
     plt.ylabel('Sum of Execution Times')
@@ -157,8 +193,11 @@ def main():
     plt.grid(axis='y')
 
     # Show plot
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
+    # plt.show()
+
+    # Save the plot to an image file
+    plt.savefig('sum_exec_time_models.png', bbox_inches="tight")
 
     # Group by 'Model' and 'CPUs_used' and sum up the 'Execution_time' for each combination
     sum_exec_time_models_cpus = results_df.groupby(['Model', 'CPUs_used'])['Execution_time'].sum()
@@ -167,7 +206,7 @@ def main():
     sum_exec_time_models_cpus = sum_exec_time_models_cpus.unstack(level=0)
 
     # Plotting
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(16, 8))
     sum_exec_time_models_cpus.plot(kind='bar', stacked=False)
     plt.xlabel('CPUs Used')
     plt.ylabel('Sum of Execution Times')
@@ -176,10 +215,19 @@ def main():
     plt.legend(title='Models', bbox_to_anchor=(1.05, 1), loc='upper left')
 
     # Show plot
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
+    # plt.show()
 
-    print(results_df.to_string(index=False))
+    # Save the plot to an image file
+    plt.savefig('sum_exec_time_models_cpus.png', bbox_inches="tight")
+
+
+    print(summary_results_df)
+    print()
+
+    results_df.sort_values(by=['Model', 'CPUs_used'], inplace=True)
+
+    print (results_df)
 
 if __name__ == "__main__":
     main()
